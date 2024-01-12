4c90964c887dfd65369cd145793705123f5ae1bf
Dharm <dharmpat@outlook.com>
2024-01-12 01:45:04 -0500

    Complete project commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '19866e7f-a3cf-4a56-b4e7-4bd7fab18515';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
